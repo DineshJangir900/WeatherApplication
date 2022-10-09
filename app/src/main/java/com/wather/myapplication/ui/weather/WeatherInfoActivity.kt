e6e89cb04f5971f8bec110ee3893202a06eae2f5
DineshJangir900 <dineshjangir900@gmail.com>
2022-10-09 17:57:37 +0530

    Initial commit

---
+++ app/src/main/java/com/wather/myapplication/ui/weather/WeatherInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.wather.myapplication.ui.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wather.myapplication.R
+
+class WeatherInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather_info)
+    }
+}
---
+++ app/src/main/java/com/wather/myapplication/ui/weather/adapters/WeatherInfoAdapter.kt
@@ -0,0 +1,24 @@
+package com.wather.myapplication.ui.weather.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class WeatherInfoAdapter : RecyclerView.Adapter<WeatherInfoAdapter.WeatherInfoViewHolder>() {
+
+    class WeatherInfoViewHolder : RecyclerView.ViewHolder(){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeatherInfoViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: WeatherInfoViewHolder, position: Int) {
+
+    }
+
+
+    override fun getItemCount(): Int {
+       return 0
+    }
+}
